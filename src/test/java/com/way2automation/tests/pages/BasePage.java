12e99225af6bd8409eb1c5dfc74ddfe894b9c580
daryamals <daryamals@mail.ru>
2025-10-09 18:48:37 +0300

    +- final

---
+++ src/test/java/com/way2automation/tests/pages/BasePage.java
@@ -0,0 +1,27 @@
+package com.way2automation.tests.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public abstract class BasePage {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        PageFactory.initElements(driver, this);
+    }
+
+    protected void waitForUrlToContain(String urlPart) {
+        wait.until(ExpectedConditions.urlContains(urlPart));
+    }
+
+    public String getCurrentUrl() {
+        return driver.getCurrentUrl();
+    }
+}
