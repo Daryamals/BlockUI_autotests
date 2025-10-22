2d0dda70cb41d70bf4f82ecf9b0a7bb8642255b3
daryamals <daryamals@mail.ru>
2025-10-22 17:22:16 +0300

    change test class

--- src/test/java/com/way2automation/tests/SqlExCookieTest.java
+++ src/test/java/com/way2automation/tests/SqlExCookieTest.java
@@ -11,41 +11,43 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.io.File;
+
 @Epic("Авторизация")
 @Feature("Авторизация через Cookies на Sql-Ex.ru")
 public class SqlExCookieTest extends BaseTest {
 
     private static final Logger logger = LoggerFactory.getLogger(SqlExCookieTest.class);
-    private final String COOKIE_FILE_PATH = "target/cookies.data";
+    private static final String COOKIE_FILE_PATH = "target/cookies.data";
+    private SqlExPage sqlExPage;
 
     @BeforeMethod
     @Override
     public void setUp() {
         super.setUp();
-    }
-
-    @Test(priority = 1, dataProvider = "sqlExLoginData", dataProviderClass = SqlExLoginProvider.class, description = "Авторизация по логину/паролю и сохранение кук")
-    @Story("Первичная авторизация")
-    @Severity(SeverityLevel.CRITICAL)
-    public void testLoginAndSaveCookies(String username, String password) {
         driver.get(TestConfig.getSqlExUrl());
-        SqlExPage sqlExPage = new SqlExPage(driver);
-        logger.info("Выполнение входа по логину/паролю для сохранения кук.");
-        sqlExPage.login(username, password);
-        Assert.assertTrue(sqlExPage.isUserLoggedIn(), "Ошибка входа по логину/паролю!");
-        CookieManager.saveCookies(driver, COOKIE_FILE_PATH);
-        logger.info("Куки успешно сохранены в {}", COOKIE_FILE_PATH);
+        sqlExPage = new SqlExPage(driver);
+        File cookieFile = new File(COOKIE_FILE_PATH);
+        if (!cookieFile.exists()) {
+            logger.info("Файл с куками не найден. Выполняется первичная авторизация для создания кук.");
+            Object[][] data = SqlExLoginProvider.sqlExLoginData();
+            String username = (String) data[0][0];
+            String password = (String) data[0][1];
+            sqlExPage.login(username, password);
+            Assert.assertTrue(sqlExPage.isUserLoggedIn(), "Ошибка входа по логину/паролю при создании кук!");
+            CookieManager.saveCookies(driver, COOKIE_FILE_PATH);
+            logger.info("Куки для авторизации успешно созданы и сохранены.");
+        }
     }
 
-    @Test(priority = 2, description = "Авторизация с использованием сохраненных кук", dependsOnMethods = "testLoginAndSaveCookies")
+    @Test(description = "Авторизация с использованием сохраненных кук", dataProvider = "sqlExLoginData", dataProviderClass = SqlExLoginProvider.class)
     @Story("Вход с использованием сохраненных куков")
     @Severity(SeverityLevel.CRITICAL)
-    public void testLoginWithCookies() {
-        driver.get(TestConfig.getSqlExUrl());
+    public void testLoginWithCookies(String username, String password) {
+        logger.info("Загрузка кук из файла: {}", COOKIE_FILE_PATH);
         driver.manage().deleteAllCookies();
         CookieManager.loadCookies(driver, COOKIE_FILE_PATH);
         driver.navigate().refresh();
-        SqlExPage sqlExPage = new SqlExPage(driver);
         Assert.assertTrue(sqlExPage.isUserLoggedIn(), "Не удалось войти с использованием Cookies!");
         logger.info("Успешный вход с использованием Cookies!");
     }
