b7e8888874097cbc1c679c27beaa482b4a343d1c
daryamals <daryamals@mail.ru>
2025-10-28 09:41:10 +0300

    fix JavaExecutorTests

--- src/test/java/com/way2automation/tests/JavaScriptExecutorTests.java
+++ src/test/java/com/way2automation/tests/JavaScriptExecutorTests.java
@@ -35,27 +35,21 @@ public class JavaScriptExecutorTests extends BaseTest {
         profilePage.fillName("Test User");
         WebElement nameInput = profilePage.getNameInput();
         Assert.assertEquals(driver.switchTo().activeElement(), nameInput, "Предусловие не выполнено: поле 'Name' не в фокусе.");
+
         pageHelper.blurActiveElement();
+
         String activeElementTag = driver.switchTo().activeElement().getTagName();
         Assert.assertEquals(activeElementTag.toLowerCase(), "body", "Фокус не был убран с поля ввода.");
     }
 
-    @Test(description = "Проверка, что JS-хелпер корректно определяет появление скролла")
+    @Test(description = "Проверка, что JS-хелпер корректно определяет изменение состояния скролла")
     @Story("JS: Определение наличия скролла")
     @Severity(SeverityLevel.NORMAL)
-    public void testScrollbarAppears() {
+    public void testScrollbarStateChanges() {
         pageHelper.ensureScrollbarIsAbsent();
         Assert.assertFalse(pageHelper.isVerticalScrollPresent(), "Предусловие нарушено: не удалось убрать скролл.");
         pageHelper.ensureScrollbarIsPresent();
         Assert.assertTrue(pageHelper.isVerticalScrollPresent(), "JS-хелпер не определил появление скролла.");
-    }
-
-    @Test(description = "Проверка, что JS-хелпер корректно определяет изчезновение скролла")
-    @Story("JS: Определение наличия скролла")
-    @Severity(SeverityLevel.NORMAL)
-    public void testScrollbarDisappears() {
-        pageHelper.ensureScrollbarIsPresent();
-        Assert.assertTrue(pageHelper.isVerticalScrollPresent(), "Предусловие нарушено: не удалось создать скролл.");
         pageHelper.ensureScrollbarIsAbsent();
         Assert.assertFalse(pageHelper.isVerticalScrollPresent(), "JS-хелпер не определил исчезновение скролла.");
     }
